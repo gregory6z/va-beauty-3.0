3c3281a4935a870220f98d7827fb30a12b8ada4a
gregory6z <gregoryrag@gmail.com>
2024-02-03 11:24:36 +0100

    Add fonts: Montserrat and Baskervvile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import { Baskervville } from "next/font/google"
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -8,13 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        serif: ["var(--font-baskervville)"],
+        sans: ["var(--font-montserrat)"],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
