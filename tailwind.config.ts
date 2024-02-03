2ffae427a642b04da1408ce55182d1663e228a0c
gregory6z <gregoryrag@gmail.com>
2024-02-03 11:43:16 +0100

    Add colors:tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         serif: ["var(--font-baskervville)"],
         sans: ["var(--font-montserrat)"],
       },
+      colors: {
+        "gray-bgdark-primary": "#080A0D",
+        "gray-bgdark-secundary": "#171717",
+
+        "gray-bglight-primary": "#F8F9FA",
+
+        "pink-highlight": "#FF8DED",
+
+        "purple-bg-primary": "#393345",
+
+        "purple-shadow-primary": "#1E163A",
+
+        "purple-text-primary": "#2E006C",
+      },
     },
   },
   plugins: [],
