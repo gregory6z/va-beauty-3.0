a3f20cfe5fb2c8014e578193f93150f8480e695c
gregory6z <gregoryrag@gmail.com>
2024-02-09 16:06:55 +0100

    update: sourcils and banner component

---
+++ src/app/components/banner/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import { ReactNode } from "react"
+
+interface BannerProps {
+  image: string
+  children: ReactNode
+}
+
+export function Banner({ image, children }: BannerProps) {
+  return (
+    <>
+      <div className="relative flex h-full w-full bg-[#020202]">
+        <div className=" mx-auto flex  w-full flex-col items-center  lg:h-[25.5rem]  lg:flex-row xl:max-w-[1256px]">
+          <div className=" h-full w-full ">
+            <Image
+              className=" bottom-0  lg:absolute lg:left-0 lg:h-[408px] "
+              width={792}
+              height={408}
+              alt={""}
+              src={image}
+            ></Image>
+          </div>
+          <div className="">
+            <h1 className=" relative order-1 mx-[1.5rem] py-5 text-center text-xl text-gray-100 sm:text-3xl lg:py-0 lg:text-right lg:text-4xl">
+              {children}
+            </h1>
+
+            <h1 className="hidden lg:block">{children}</h1>
+          </div>
+        </div>
+      </div>
+      {/* <div className="flex h-full w-full bg-black ">
+        <h1 className=" text-bold mx-6 my-4 text-center text-xl text-white lg:hidden">
+          {children}
+        </h1>
+      </div> */}
+    </>
+  )
+}
---
+++ src/app/sourcils/page.tsx
@@ -0,0 +1,12 @@
+import { Banner } from "../components/banner"
+
+export default function Sourcils() {
+  return (
+    <div className="bg-[#F8F9FA]">
+      <Banner image={"/sourcils-1.png"}>
+        Sublimez votre beauté naturelle en sculptant vos sourcils avec
+        perfection.
+      </Banner>
+    </div>
+  )
+}
