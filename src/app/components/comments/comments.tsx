893bc164b7793d61b60b2c4023f2a1bec6cbeefc
gregory6z <gregoryrag@gmail.com>
2024-05-19 16:06:29 +0200

    :heavy_check_mark: fix : add comment dani

--- src/app/components/comments/comments.tsx
+++ src/app/components/comments/comments.tsx
@@ -1,26 +1,26 @@
 export const CommentsHomeFirstPart = [
   {
+    name: "Daniela Benites",
+    content: `Après une séance d'hydratation des lèvres, ma bouche est plus hydratée que jamais ! La sensation est incroyable et les résultats sont visibles immédiatement. Valesca sait vraiment comment rendre mes lèvres douces et hydratées, tout en offrant une expérience relaxante et revigorante. C'est comme un traitement spa pour les lèvres ! Je suis absolument enchantée par le résultat et je vais certainement revenir pour plus de séances d'hydratation avec Valesca.`,
+    image: "dani.jpg",
+  },
+  {
     name: "Rafaela Marinho",
     content: `Une séance de soin des sourcils avec Valesca, c'est juste top ! Elle a vraiment le sens du détail et une expertise qui font toute la différence. Elle sait comment sublimer vos sourcils tout en vous mettant super à l'aise et vous faisant sentir super bien. Après chaque rendez-vous avec Valesca, je me sens juste magnifique à l'extérieur, mais aussi super boostée à l'intérieur. Franchement, c'est un pur bonheur de profiter de ses services !`,
     image: "/rafaella.jpeg",
-  },
-  {
-    name: "Caliana Camossa",
-    content: `La micropigmentation réalisée par Valesca a été géniale ! Mes sourcils sont impeccables et je me sens plus confiante que jamais. Valesca est une véritable artiste et je suis vraiment ravie du résultat.`,
-    image: "/caliana.jpeg",
   },
 ]
 
 export const CommentsHomeSecondPart = [
   {
+    name: "Caliana Camossa",
+    content: `La micropigmentation réalisée par Valesca a été géniale ! Mes sourcils sont impeccables et je me sens plus confiante que jamais. Valesca est une véritable artiste et je suis vraiment ravie du résultat.`,
+    image: "/caliana.jpeg",
+  },
+  {
     name: "Natacha Alves",
     content: `La micropigmentation des lèvres réalisée par Valesca était juste incroyable ! Mes lèvres ont maintenant une teinte plus rosée et naturelle, et je suis vraiment ravie du résultat. Valesca est une véritable experte dans son domaine, et sa capacité à sublimer la beauté naturelle est remarquable. J'ai hâte de montrer mes nouvelles lèvres partout où je vais !`,
     image: "/natacha.png",
-  },
-  {
-    name: "--/--",
-    content: `Après une séance d'hydratation des lèvres, ma bouche est plus hydratée que jamais ! La sensation est incroyable et les résultats sont visibles immédiatement. Valesca sait vraiment comment rendre mes lèvres douces et hydratées, tout en offrant une expérience relaxante et revigorante. C'est comme un traitement spa pour les lèvres ! Je suis absolument enchantée par le résultat et je vais certainement revenir pour plus de séances d'hydratation avec Valesca.`,
-    image: "/--/--.png",
   },
 ]
 export const CommentsHome = [
